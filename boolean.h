0343dbb7ae4f747ede4f72bfb8ce2ebfc76c35a3
zahwanazalla16 <zahwa.nazalla@gmail.com>
2025-05-06 15:26:02 +0700

    push boolean

---
+++ boolean.h
@@ -0,0 +1,11 @@
+#ifndef boolean_H
+#define boolean_H
+
+#define boolean unsigned char
+#define true 1
+#define false 0
+
+#endif
+
+// ganti Idx, Idx adalah indeks
+// benerin dan rapihkan tampilan 
